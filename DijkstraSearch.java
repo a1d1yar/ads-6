71763ffd8d777ed4c5038e0a0c4cf050a563efb1
Aldiyar <aldiyarmaratov0@gmail.com>
2023-06-01 00:39:27 +0600

    added doc for DijkstraSearch

--- DijkstraSearch.java
+++ DijkstraSearch.java
@@ -1,10 +1,27 @@
 import java.util.Map;
+
+/**
+ * Implementation of the Dijkstra's algorithm for finding the shortest paths in a graph.
+ *
+ * @param <V> the type of data stored in the vertices
+ */
 public class DijkstraSearch<V> implements Search<V> {
     private WeightedGraph<V> graph;
+
+    /**
+     * Creates a new instance of DijkstraSearch with the specified graph.
+     *
+     * @param graph the weighted graph to perform the search on
+     */
     public DijkstraSearch(WeightedGraph<V> graph) {
         this.graph = graph;
     }
 
+    /**
+     * Performs Dijkstra's algorithm starting from the given vertex and prints the distances to all other vertices.
+     *
+     * @param start the starting vertex for the search
+     */
     @Override
     public void search(Vertex<V> start) {
         Map<Vertex<V>, Double> distances = graph.Dijkstra(start);
